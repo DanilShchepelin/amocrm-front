c096d805f8fdade91e71c198d38cc3210c3d537e
Щепелин Данил <schepelind@echo-company.ru>
2024-06-19 10:42:28 +0500

    исправил баг

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import { Input, Table } from "ant-design-vue";
+import {Card, Input, Table} from "ant-design-vue";
 
 const app = createApp(App)
 
 app.use(Table)
 app.use(Input)
+app.use(Card)
 
 app.mount('#app')
--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,37 +1,39 @@
 <template>
-  <a-input-search
-      v-model:value="value"
-      placeholder="input search text"
-      style="width: 200px"
-      @search="onSearch"
-  />
-  <a-table :columns="columns" :data-source="dataSource" :scroll="{ x: 2000 }" :pagination="false">
-    <template #bodyCell="{ column, value }">
-      <template v-if="column.key === 'status'">
+  <a-card style="margin: 0 auto;max-width: 1560px;">
+    <a-input-search
+        v-model:value="value"
+        placeholder="Поиск..."
+        style="width: 200px; margin-bottom: 20px"
+        @search="onSearch"
+    />
+    <a-table :columns="columns" :data-source="dataSource" :scroll="{ x: 400 }" :pagination="false">
+      <template #bodyCell="{ column, value }">
+        <template v-if="column.key === 'status'">
         <span style="padding: 5px; border-radius: 5px;" :style="{ background: value.color }">
           {{ value?.name }}
         </span>
+        </template>
+        <template v-else-if="column.key === 'responsible'">
+          {{ value?.name }}
+        </template>
       </template>
-      <template v-else-if="column.key === 'responsible'">
-        {{ value?.name }}
-      </template>
-    </template>
-    <template #expandedRowRender="{ record }">
-      <div v-for="contact in record.contacts" :key="contact.id" >
-        <div>{{contact.name}}</div>
-        <div v-if="contact?.phones?.length > 0">
-          <span v-if="contact.phones.length > 1" style="margin-top: 5px">Телефоны</span>
-          <span v-else style="margin-top: 5px">Телефон</span>
-          <a v-for="phone in contact.phones" :key="phone.value" href="tel:'`${phone.value}`" style="margin-top: 3px; display: block">{{phone.value}}</a>
-        </div>
+      <template #expandedRowRender="{ record }">
+        <div v-for="contact in record.contacts" :key="contact.id" >
+          <div>{{contact.name}}</div>
+          <div v-if="contact?.phones?.length > 0">
+            <span v-if="contact.phones.length > 1" style="margin-top: 5px">Телефоны</span>
+            <span v-else style="margin-top: 5px">Телефон</span>
+            <a v-for="phone in contact.phones" :key="phone.value" href="tel:'`${phone.value}`" style="margin-top: 3px; display: block">{{phone.value}}</a>
+          </div>
 
-        <div v-if="contact?.emails?.length > 0">
-          <span style="margin-top: 5px">Email</span>
-          <a v-for="email in contact.emails" :key="email.value" href="mailto:'`${email.value}`" style="margin-top: 3px; display: block">{{email.value}}</a>
+          <div v-if="contact?.emails?.length > 0">
+            <span style="margin-top: 5px">Email</span>
+            <a v-for="email in contact.emails" :key="email.value" href="mailto:'`${email.value}`" style="margin-top: 3px; display: block">{{email.value}}</a>
+          </div>
         </div>
-      </div>
-    </template>
-  </a-table>
+      </template>
+    </a-table>
+  </a-card>
 </template>
 
 <script lang="ts">
@@ -39,7 +41,7 @@ import axios from "axios";
 import {defineComponent, onMounted, ref} from "vue";
 
 interface DataItem {
-  id: number;
+  key: number;
   name: string;
   price: number;
   createdAt: string;
@@ -58,13 +60,12 @@ interface Contacts {
   emails: Record<string, string>[]
 }
 
-
 export default defineComponent({
   setup() {
     const value = ref<string>('');
     const dataSource = ref([] as DataItem[]);
     const columns = [
-      { title: 'Название', dataIndex: 'name', key: 'name', fixed: true },
+      { title: 'Название', dataIndex: 'name', key: 'name' },
       { title: 'Бюджет', dataIndex: 'price', key: 'price' },
       { title: 'Статус', dataIndex: 'status', key: 'status' },
       { title: 'Ответственный', dataIndex: 'responsibleUser', key: 'responsible' },
